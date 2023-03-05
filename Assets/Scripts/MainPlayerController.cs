ade44460ac56f626f3b6cd5192bc46af4484ee10
IdealSang <126327750+IdealSang@users.noreply.github.com>
2023-03-06 00:05:20 +0900

    작업중

---
+++ Assets/Scripts/MainPlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed;
+    public float h;
+    public float v;
+    private Rigidbody2D rigid;
+    void Awake()
+    {
+    }
+
+}
