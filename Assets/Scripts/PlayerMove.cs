22e7e2a27c13a0111bb7a48984fb7a5ca86476fa
IdealSang <126327750+IdealSang@users.noreply.github.com>
2023-03-06 23:48:54 +0900

    Setting

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerAction : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float speed;
     public float h;
     public float v;
-    private Rigidbody2D rigid;
     void Awake()
     {
     }
+    void Update()
+    {
 
+    }
 }
